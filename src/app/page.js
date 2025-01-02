1490bdab1841c66297271efb066162f257e580b6
Shadman Shaikh <shadmanforreal45@gmail.com>
2025-01-02 21:55:01 +0400

    changed positioning of text in the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <p className="text-2xl">Main Page</p>
+      <p className="text-2xl text-center h-screen">Main Page</p>
    </div>
   );
 }
